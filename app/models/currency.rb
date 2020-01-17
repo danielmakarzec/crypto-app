0bf54522617eed4e6da2b7f756adfd5bcd1ce84e
daniel makarzec <danielmakarzec@gmail.com>
2020-01-17 12:43:17 +0000

    create currency current_price method

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,7 @@
 class Currency < ApplicationRecord
+  def current_price
+    url = 'http://api.coinmarketcap.com/v1/ticker/'
+    request = HTTParty.get(url + self.slug)
+    response = JSON.parse(request.body)[0]['price_usd']
+  end
 end
