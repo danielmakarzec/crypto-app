c31f731f7c98ee23313ff90d236c9c2491ab7b0f
daniel makarzec <danielmakarzec@gmail.com>
2020-01-17 13:20:55 +0000

    create the method that calculates the value of given currency based on amount in the portfolio

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,7 +1,11 @@
 class Currency < ApplicationRecord
   def current_price
     url = 'http://api.coinmarketcap.com/v1/ticker/'
-    request = HTTParty.get(url + self.slug)
-    response = JSON.parse(request.body)[0]['price_usd']
+    request = HTTParty.get(url + slug)
+    JSON.parse(request.body)[0]['price_usd']
+  end
+
+  def calculate_value(amount)
+    (current_price.to_f * amount.to_f).round(4)
   end
 end
